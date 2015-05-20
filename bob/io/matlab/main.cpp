61580306eaba05b4225281eda06799f0b3408c9b
Manuel Guenther <manuel.guenther@idiap.ch>
2015-05-20 11:00:00 +0200

    Removed duplicated constants from c module

--- bob/io/matlab/main.cpp
+++ bob/io/matlab/main.cpp
@@ -173,9 +173,6 @@ static PyObject* create_module (void) {
   if (!m) return 0;
   auto m_ = make_safe(m);
 
-  /* register some constants */
-  if (PyModule_AddStringConstant(m, "__version__", BOB_EXT_MODULE_VERSION) < 0) return 0;
-
   /* imports dependencies */
   if (import_bob_blitz() < 0) {
     PyErr_Print();
