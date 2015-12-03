853dddeb5049c85fb864570c6de689ee0d25df8f
Manuel Gunther <siebenkopf@googlemail.com>
2015-12-03 13:24:44 -0700

    Corrected error cases in module creation

--- bob/io/matlab/main.cpp
+++ bob/io/matlab/main.cpp
@@ -168,11 +168,13 @@ static PyObject* create_module (void) {
 
 # if PY_VERSION_HEX >= 0x03000000
   PyObject* m = PyModule_Create(&module_definition);
+  auto m_ = make_xsafe(m);
+  const char* ret = "O";
 # else
   PyObject* m = Py_InitModule3(BOB_EXT_MODULE_NAME, module_methods, module_docstr);
+  const char* ret = "N";
 # endif
   if (!m) return 0;
-  auto m_ = make_safe(m);
 
   /* imports dependencies */
   if (import_bob_blitz() < 0) return 0;
@@ -185,7 +187,7 @@ static PyObject* create_module (void) {
     //do not return 0, or we may crash badly
   }
 
-  return Py_BuildValue("O", m);
+  return Py_BuildValue(ret, m);
 }
 
 PyMODINIT_FUNC BOB_EXT_ENTRY_NAME (void) {
